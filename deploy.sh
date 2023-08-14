6c4bf2a765bdd1557fdd94c64e4c0e97f232edf1
Aibek <aibek.abdykasymov@gmail.com>
2023-08-14 18:05:14 +0600

    Updated deploy script, added optional parameters, added help info

--- deploy.sh
+++ deploy.sh
@@ -63,58 +63,98 @@ function makeArchive() {
   cd -
 }
 
-build_dir="build"
-project_dir_name="project"
-project_dir="${build_dir}/${project_dir_name}"
+function deployBack() {
+  local build_dir="build"
+  local project_dir_name="project"
+  local project_dir="${build_dir}/${project_dir_name}"
 
-rm -rf $build_dir
-mkdir -p $project_dir
-
-echo $DOCKER_PASSWORD | docker login --username=${DOCKER_USER} ${DOCKER_REGISTRY} --password-stdin
-
-printLog "Building and deploying images..."
-buildAndPushImage ${CURRENT_DOW_BOT_DIRECTORY} ${DOCKER_DOW_BOT_IMAGE}
-
-printLog "Copying docker-compose files for building..."
-copyComposeFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot
-
-printLog "Copying run_docker files for building..."
-copyRunScriptFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot
-
-printLog "Copying environment variables files for building..."
-bot_env_files=(".env.production")
-copyEnvironmentFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot "${bot_env_files[@]}"
-
-printLog "Making project archive..."
-current_date=$(date +'%m-%d-%Y_%H-%M-%S')
-project_archive="project_${current_date}.tar.gz"
-makeArchive ${build_dir} ${project_dir_name} ${project_archive}
-printLog "Creating directory in remote server..."
-project_archive_file="${build_dir}/${project_archive}"
-ssh -i "${REMOTE_SSH_KEY_FILE}" -t ${REMOTE_USER}@${REMOTE_SERVER} "mkdir -p ${REMOTE_DIRECTORY}"
-printLog "Copying to remote server..."
-scp -i "${REMOTE_SSH_KEY_FILE}" ${project_archive_file} ${REMOTE_USER}@${REMOTE_SERVER}:${REMOTE_DIRECTORY}
-printLog "Deploying docker containers in remote server..."
-
-ssh -i "${REMOTE_SSH_KEY_FILE}" -t ${REMOTE_USER}@${REMOTE_SERVER} << EOF
-  cd ${REMOTE_DIRECTORY}
-
-  tar -xzvf ${project_archive}
-  rm -rf ${project_archive}
+  rm -rf $build_dir
+  mkdir -p $project_dir
 
   echo $DOCKER_PASSWORD | docker login --username=${DOCKER_USER} ${DOCKER_REGISTRY} --password-stdin
 
-  /usr/bin/bash ${project_dir_name}/dow-bot/scripts/run_docker.sh ${DOCKER_DOW_BOT_IMAGE}
+  printLog "Building and deploying images..."
+  buildAndPushImage ${CURRENT_DOW_BOT_DIRECTORY} ${DOCKER_DOW_BOT_IMAGE}
 
+  printLog "Copying docker-compose files for building..."
+  copyComposeFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot
+
+  printLog "Copying run_docker files for building..."
+  copyRunScriptFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot
+
+  printLog "Copying environment variables files for building..."
+  local bot_env_files=(".env.production")
+  copyEnvironmentFiles ${CURRENT_DOW_BOT_DIRECTORY} ${project_dir}/dow-bot "${bot_env_files[@]}"
+
+  printLog "Making project archive..."
+  local current_date=$(date +'%m-%d-%Y_%H-%M-%S')
+  local project_archive="project_${current_date}.tar.gz"
+  makeArchive ${build_dir} ${project_dir_name} ${project_archive}
+
+  printLog "Creating directory in remote server..."
+  local project_archive_file="${build_dir}/${project_archive}"
+  ssh -i "${REMOTE_SSH_KEY_FILE}" -t ${REMOTE_USER}@${REMOTE_SERVER} "mkdir -p ${REMOTE_DIRECTORY}"
+
+  printLog "Copying to remote server..."
+  scp -i "${REMOTE_SSH_KEY_FILE}" ${project_archive_file} ${REMOTE_USER}@${REMOTE_SERVER}:${REMOTE_DIRECTORY}
+  printLog "Deploying docker containers in remote server..."
+
+  ssh -i "${REMOTE_SSH_KEY_FILE}" -t ${REMOTE_USER}@${REMOTE_SERVER} << EOF
+    cd ${REMOTE_DIRECTORY}
+
+    tar -xzvf ${project_archive}
+    rm -rf ${project_archive}
+
+    echo $DOCKER_PASSWORD | docker login --username=${DOCKER_USER} ${DOCKER_REGISTRY} --password-stdin
+
+    /usr/bin/bash ${project_dir_name}/dow-bot/scripts/run_docker.sh ${DOCKER_DOW_BOT_IMAGE}
 EOF
 
-printLog "Deploy to cloudflare pages"
-cd ${CURRENT_DOW_DASH_DIRECTORY}
-  [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
-  nvm use
-  /usr/bin/bash ./scripts/deploy.sh ${CLOUDFLARE_ACCOUNT_ID} ${CLOUDFLARE_API_TOKEN} ${CLOUDFLARE_PAGE_PROJECT_NAME}
-cd -
+  printLog "Cleaning unused docker build cache objects"
+  docker system prune -f
+}
 
-printLog "Cleaning unused docker build cache objects"
-docker system prune -f
+function deployFront() {
+  printLog "Deploy to cloudflare pages"
+  cd ${CURRENT_DOW_DASH_DIRECTORY}
+    [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+    nvm use
+    /usr/bin/bash ./scripts/deploy.sh ${CLOUDFLARE_ACCOUNT_ID} ${CLOUDFLARE_API_TOKEN} ${CLOUDFLARE_PAGE_PROJECT_NAME}
+  cd -
+}
+
+function printHelp() {
+  echo \
+"Deploy dowdash bot script:
+   Syntax: ./deploy [--front|--back|--help]
+   options:
+   --front  deploy dow-dash to cloudflare pages
+   --back   deploy dow-bot to remote server
+   --help   show help
+If run without options front and back will be deployed."
+}
+
+if [[ "$*" =~ .*"--help".* ]]
+then
+  printHelp
+  exit 0
+fi
+
+if [ -z "$*" ]
+then
+  deployBack
+  deployFront
+else
+  for i in $*;
+    do
+      if [[ "$i" == "--back" ]]
+      then
+        deployBack
+      elif  [[ "$i" == '--front' ]]
+      then
+        deployFront
+      fi
+    done
+fi
+
 printLog "Finished deploy"
